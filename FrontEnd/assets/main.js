8092455794f7e0892b9b3f813947bba75bb680f7
guilhem durbal <gdurbal@gmail.com>
2024-01-20 17:28:18 +0100

    ajout de login

--- FrontEnd/assets/main.js
+++ FrontEnd/assets/main.js
@@ -1,57 +1,84 @@
-  async function postData(url = "", data = {}) {
+
+
+async function getData(url = '', data = {}) {
   const response = await fetch(url, {
-  method: "GET",
-  mode: "cors",
-  cache: "no-cache",
-  credentials: "same-origin",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  redirect: "follow",
-  referrerPolicy: "no-referrer",
-  //body: JSON.stringify(data),
-  });
-  return response.json();
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    //body: JSON.stringify(data),
+  })
+  return response.json()
 }
 
-postData("http://localhost:5678/api/works", {
+getData('http://localhost:5678/api/works', {
   //"email": "sophie.bluel@test.tld",
   //"password": "S0phie",
   //"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiO…DI4fQ.2dkOL2Vz8HdgFxha2xzm1RmJjehohb8J3uHWcirGqHk"
 }).then((data) => {
-  let typesUniques = new Set();
+  let typesUniques = new Set()
   for (let i = 0; i < data.length; i++) {
-    const element = data[i];
+    const element = data[i]
     //console.log(element);
-    const projects = document.querySelector("#projects");
-    const figure = '<figure><img src="' + element.imageUrl + '" alt="' + element.title +'"><figcaption> ' + element.title +' </figcaption></figure>';
-    projects.innerHTML += figure;
+    const projects = document.querySelector('#projects')
+    projects.innerHTML += renderCard(element)
     typesUniques.add(data[i].category.name)
   }
-  //console.log(typesUniques);
-});
 
+  console.log(typesUniques)
+})
+
+async function init() {
+  const categories = getData('http://localhost:5678/api/categories').then(
+    (data) => {
+      const filterZone = document.querySelector('.filter-zone')
+      filterZone.innerHTML = ''
+      filterZone.innerHTML += `<p><a href="">Tous</a></p>`
+      data.forEach((element) => {
+        filterZone.innerHTML += `<p><button onclick="filterWorks('` + element.name + `')"> ` + element.name + `</button></p>`
+      })
+    }
+  )
+}
 
 function filterWorks(type) {
-  postData("http://localhost:5678/api/works", {
-    }).then((data) => {
-      const projects = document.querySelector("#projects");
-      projects.innerHTML = "";
-      if (type === "Tous") {
-        for (let i = 0; i < data.length; i++) {
-          const element = data[i];
-          const figure = '<figure><img src="' + element.imageUrl + '" alt="' + element.title +'"><figcaption> ' + element.title +' </figcaption></figure>';
-          projects.innerHTML += figure;
-        }
-      } else {
-        const filteredData = data.filter((element) => element.category.name === type);
-        for (let i = 0; i < filteredData.length; i++) {
-          const element = filteredData[i];
-          const figure = '<figure><img src="' + element.imageUrl + '" alt="' + element.title +'"><figcaption> ' + element.title +' </figcaption></figure>';
-          projects.innerHTML += figure;
-        }
+  getData('http://localhost:5678/api/works', {}).then((data) => {
+    const projects = document.querySelector('#projects')
+    projects.innerHTML = ''
+    if (type === 'Tous') {
+      for (let i = 0; i < data.length; i++) {
+        const element = data[i]
+        projects.innerHTML += renderCard(element)
       }
-  });
- // Mettre à jour le HTML avec les éléments filtrés
- 
+    } else {
+      const filteredData = data.filter(
+        (element) => element.category.name === type
+      )
+      for (let i = 0; i < filteredData.length; i++) {
+        const element = filteredData[i]
+
+        projects.innerHTML += renderCard(element)
+      }
+    }
+  })
+  // Mettre à jour le HTML avec les éléments filtrés
 }
+
+function renderCard(element) {
+  const figure =
+    '<figure><img src="' +
+    element.imageUrl +
+    '" alt="' +
+    element.title +
+    '"><figcaption> ' +
+    element.title +
+    ' </figcaption></figure>'
+  return figure
+}
+
+init();
